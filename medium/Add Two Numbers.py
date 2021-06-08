eeddf5bd482386b49b6150ae5d01caa057f7089b
QbeeQ <alexey.vu@gmail.com>
2021-06-08 13:42:59 +0300

    decided "Add Two Numbers"

--- medium/Add Two Numbers.py
+++ medium/Add Two Numbers.py
@@ -26,36 +26,81 @@ class ListNode:
         self.next = next
 
     def __str__(self) -> str:
-        result = str(self.val)
+        result = "["+str(self.val)
         el = self.next
         while el:
-            result = f"{result} -> {str(el.val)}"
+            result = f"{result}{str(el.val)}"
             el = el.next
-        return result
+        return result+"]"
+
+    def __eq__(self, o: object) -> bool:
+        if not isinstance(o, ListNode): return False
+        if self.val != o.val: return False
+        return self.next == o.next
 
 class Solution:
+    def addTwoNumbers_off(self, l1: ListNode, l2: ListNode, up=None) -> ListNode:
+        if l1 or l2 or up:
+            s = (0 if l1 is None else l1.val) + (0 if l2 is None else l2.val) + (0 if up is None else up)
+            res = ListNode(s if s < 10 else s-10)
+            res.next = self.addTwoNumbers(
+                l1.next if l1 else None,
+                l2.next if l2 else None,
+                None if s < 10 else 1
+            )
+            return res
+
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        pass
+        res = cur = None
+        up = 0
+        while l1 or l2 or up > 0:
+            if res is None:
+                res = cur = ListNode((0 if l1 is None else l1.val) + (0 if l2 is None else l2.val) + up)
+            else:
+                cur.next = ListNode((0 if l1 is None else l1.val) + (0 if l2 is None else l2.val) + up)
+                cur = cur.next
+            if cur.val < 10:
+                up = 0
+            else:
+                up = 1
+                cur.val -= 10
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
+        return res
 
 class Tester:
     def __init__(self) -> None:
+        self.res = ("FAIL", "PASS")
         self.examples = [
-            ([2,4,3], [5,6,4], [7,0,8]),
-            ([0], [0], [0]),
-            ([9,9,9,9,9,9,9], [9,9,9,9], [8,9,9,9,0,0,0,1])
+            (
+                ListNode(2, ListNode(4, ListNode(3))),
+                ListNode(5, ListNode(6, ListNode(4))),
+                ListNode(7, ListNode(0, ListNode(8)))
+            ),
+            (
+                ListNode(0),
+                ListNode(0),
+                ListNode(0)
+            ),
+            (
+                ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9))))))),
+                ListNode(9, ListNode(9, ListNode(9, ListNode(9)))),
+                ListNode(8, ListNode(9, ListNode(9, ListNode(9, ListNode(0, ListNode(0, ListNode(0, ListNode(1))))))))
+            )
         ]
-    
+
     def compare(self, lst1:ListNode, lst2:ListNode) -> bool:
         return str(lst1) == str(lst2)
 
     def test(self) -> bool:
         all_tests_result = True
         for l1, l2, reference in self.examples:
-            result = Solution.addTwoNumbers(l1, l2)
+            result = Solution().addTwoNumbers(l1, l2)
             test_result = self.compare(reference, result)
             all_tests_result = min(all_tests_result, test_result)
-            print(f"{'PASS' if test_result else 'FAIL'}\ttwoSum({nums}, {target}) = {result} (reference: {reference})")
+            print(f"{self.res[test_result]}\taddTwoNumbers({l1}, {l2}) = {result} (reference: {reference})")
         return all_tests_result
 
+
 t  = Tester()
-t.test()
+print(f"Test result: {t.res[t.test()]}")
